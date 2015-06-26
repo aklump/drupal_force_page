40ee814fa4d68539ad5ce513fae90da44e716dd9
aklump <sourcecode@intheloftstudios.com>
2015-06-26 16:29:36 -0700

    Added hook_force_page_allowed_paths_alter. Added support for masquerade/unswitch.

--- force_page.api.php
+++ force_page.api.php
@@ -21,16 +21,16 @@
  * - Values of the array should be arrays with following keys:
  *  - weight: The lowest weight will take precendence
  *  - test: If this is TRUE the user will be redirected to the array key. If it
-    is FALSE, this forced page will be overlooked. You would usually use the
-    $account variable and feed it to some function that returns a boolean value
-    here; however you can hardcode values as well, or mathmatical expressions,
-    e.g. time() > 1307744520
-    - test_arguments: This array will be sent to the test callback
-    - message: a message to display if the redirection occurs, this must be
-      sanitized as it will be output directly
-    - allowed_paths: an array of paths that will be allowed to be visited; you
-      may also use <front> for the site_frontpage variable
-      without forcing a redirection, such as "logout"
+ *  is FALSE, this forced page will be overlooked. You would usually use the
+ *  $account variable and feed it to some function that returns a boolean value
+ *  here; however you can hardcode values as well, or mathmatical expressions,
+ *  e.g. time() > 1307744520
+ *  - test_arguments: This array will be sent to the test callback
+ *  - message: a message to display if the redirection occurs, this must be
+ *    sanitized as it will be output directly
+ *  - allowed_paths: an array of paths that will be allowed to be visited; you
+ *    may also use <front> for the site_frontpage variable
+ *    without forcing a redirection, such as "logout"
  */
 function hook_force_page_pages($account) {
 
@@ -57,6 +57,13 @@ function hook_force_page_pages($account) {
       ),
     ),
   );
+}
+
+/**
+ * Implements hook_force_page_allowed_paths_alter().
+ */
+function hook_force_page_allowed_paths_alter($allowed_paths, $current_path) {
+
 }
 
 /** @} */ //end of group force_page
